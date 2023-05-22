b89967b047d72c5c052dfc121982e441401e81f9
Siyuan <SILU@simcorp.com>
2023-05-22 15:36:00 +0200

    SILU 2

--- MyConsoleApplication/Program.cs
+++ MyConsoleApplication/Program.cs
@@ -12,6 +12,7 @@ namespace MyConsoleApplication
         {
             Console.WriteLine("Hello World.");
             Console.WriteLine("Feature branch 1 - development 1");
+            Console.WriteLine("Feature branch 1 - development 2");
             Console.ReadKey();
         }
     }
