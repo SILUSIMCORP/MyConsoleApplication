388819f5210000877bfcb2457cbfbf6b52d2aa9d
SILUSIMCORP <134050578+SILUSIMCORP@users.noreply.github.com>
2023-05-22 16:05:51 +0200

    SILU

--- MyConsoleApplication/Program.cs
+++ MyConsoleApplication/Program.cs
@@ -11,7 +11,6 @@ namespace MyConsoleApplication
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World.");
-            Console.ReadKey();
         }
     }
 }
