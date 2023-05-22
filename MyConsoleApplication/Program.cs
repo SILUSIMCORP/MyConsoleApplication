1ba3a2dbd22bc7b7b1a478bd3ccede84692165a4
Siyuan <SILU@simcorp.com>
2023-05-22 15:45:59 +0200

    Revert "SILU"
    This reverts commit 79c45490bdad464b688f0169c2cc189afb05bc3e.

--- MyConsoleApplication/Program.cs
+++ MyConsoleApplication/Program.cs
@@ -10,9 +10,7 @@ namespace MyConsoleApplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World.");
-            Console.WriteLine("Feature branch 1 - development 1");
-            Console.WriteLine("Feature branch 1 - development 2");
+            Console.WriteLine("Hello World");
             Console.ReadKey();
         }
     }
