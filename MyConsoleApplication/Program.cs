ada26795c02c334141ea98af2600fd8270f2cd4a
Siyuan <SILU@simcorp.com>
2023-05-24 15:32:15 +0200

    Adding new branch by SILU

--- MyConsoleApplication/Program.cs
+++ MyConsoleApplication/Program.cs
@@ -8,9 +8,27 @@ namespace MyConsoleApplication
 {
     class Program
     {
+        enum TransactionType {Buy=1,Sell=-1};
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World.");
+            Console.WriteLine("Enter nominal of the trade");
+            string nominalInput = Console.ReadLine();
+            int nominal = int.Parse(nominalInput);
+
+            Console.WriteLine("Enter trade price.");
+            string priceInput = Console.ReadLine();
+            int price = int.Parse(priceInput);
+
+            Console.WriteLine("Buy or Sell?");
+            TransactionType transaction;
+            string tType = Console.ReadLine();
+            transaction = (TransactionType) Enum.Parse( typeof(TransactionType), tType, true);
+
+            int sign = tType == "Buy" ? 1: -1; 
+            int CV = nominal * price * sign;
+            Console.WriteLine($"The Currenct Value of your investment is {CV}.");
+            Console.ReadKey();
         }
     }
 }
